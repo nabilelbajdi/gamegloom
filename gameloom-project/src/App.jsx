69cf7c575e670ed980d2a1f0bd043f793c52ac2c
nabilelbajdi <113439816+nabilelbajdi@users.noreply.github.com>
2025-01-30 21:57:59 +0100

    Integrate custom CSS styles

--- gameloom-project/src/App.jsx
+++ gameloom-project/src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Header from "./components/Header"
 
 const App = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-purple-900 text-white">
-      <h1 className="text-3xl font-bold underline">Hello World!</h1>
+    <div className="min-h-screen bg-purple-900 text-white">
+      <Header />
+      <div className="min-h-screen flex items-center justify-center bg-purple-900 text-white">  
+        <h1 className="text-3xl font-bold underline">Hello World!</h1>
+      </div>
     </div>
   );
 };
