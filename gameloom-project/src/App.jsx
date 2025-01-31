621d1c434540fb8e767a5f3c068c8edeb4e806bd
nabilelbajdi <113439816+nabilelbajdi@users.noreply.github.com>
2025-01-31 20:07:27 +0100

    Add ReviewedGames section and integrate it into App.jsx

--- gameloom-project/src/App.jsx
+++ gameloom-project/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header"
 import Hero from "./components/Hero"
+import ReviewedGames from "./components/ReviewedGames"
 
 const App = () => {
   return (
     <div className="min-h-screen bg-gray-900 text-white">
       <Header />
       <Hero />
+      <ReviewedGames />
     </div>
   );
 };
