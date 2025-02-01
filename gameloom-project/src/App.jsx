ecc7c9eb5304a0fc500ffb2e59f80d6ebf184401
nabilelbajdi <113439816+nabilelbajdi@users.noreply.github.com>
2025-02-01 12:51:40 +0100

    Integrate FeaturedGames into App.jsx

--- gameloom-project/src/App.jsx
+++ gameloom-project/src/App.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Header from "./components/Header"
 import Hero from "./components/Hero"
+import FeaturedGames from "./components/FeaturedGames"
 import ReviewedGames from "./components/ReviewedGames"
 import Footer from "./components/Footer"
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-gray-900 text-white">
+    <div className="min-h-screen bg-gray-900 text-white font-body">
       <Header />
       <Hero />
+      <FeaturedGames />
       <ReviewedGames />
       <Footer />
     </div>
