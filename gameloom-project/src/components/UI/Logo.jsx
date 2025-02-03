909a391a54dddb44acbd0fbafc1fe051811391d3
nabilelbajdi <113439816+nabilelbajdi@users.noreply.github.com>
2025-02-03 18:44:42 +0100

    Add reusable Logo component

---
+++ gameloom-project/src/components/UI/Logo.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+const Logo = ({ className = "h-14 w-auto" }) => {
+  return (
+    <Link to="/" className={`logo flex items-center ${className}`}>
+      <img
+        src="/images/logo.svg"
+        alt="GameLoom Logo"
+        className="w-auto h-full object-contain"
+      />
+    </Link>
+  );
+};
+
+export default Logo;
