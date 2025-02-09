226714d79ce54508007dd76e48c8fd5fd04494e4
nabilelbajdi <113439816+nabilelbajdi@users.noreply.github.com>
2025-02-09 02:18:58 +0100

    Fix layout structure: use w-full instead of min-w-screen

--- gameloom-project/src/components/layout/Layout.jsx
+++ gameloom-project/src/components/layout/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <main className="flex-grow min-w-screen container mx-auto content-center">
+      <main className="flex-grow w-full">
         <Outlet />
       </main>
       <Footer />
