b5aadfc7985bd3c80d1af0abc1c788d2b3123a88
nabilelbajdi <113439816+nabilelbajdi@users.noreply.github.com>
2025-02-25 14:56:14 +0100

    Fix unreleased games in game library not showing N/A on rating

--- gameloom-project/backend/app/api/v1/routers/user_games.py
+++ gameloom-project/backend/app/api/v1/routers/user_games.py
@@ -77,7 +77,7 @@ async def get_user_collection(
             name=game.name,
             coverImage=game.cover_image,
             genres=game.genres,
-            rating=format(float(game.total_rating) / 20, ".1f") if game.total_rating else None,
+            rating="N/A" if not game.total_rating else format(float(game.total_rating) / 20, ".1f"),
             first_release_date=game.first_release_date,
             added_at=user_game.added_at,
             updated_at=user_game.updated_at,
