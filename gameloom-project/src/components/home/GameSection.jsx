114fb1711bfc88c38fb9e6cd6d48661e034416f4
nabilelbajdi <113439816+nabilelbajdi@users.noreply.github.com>
2025-02-28 17:15:04 +0100

    Comment out unused GameSection component

--- gameloom-project/src/components/home/GameSection.jsx
+++ gameloom-project/src/components/home/GameSection.jsx
@@ -1,20 +1,20 @@
-import React, { useEffect } from "react";
-import useGameStore from "../../store/useGameStore";
-import GameGrid from "../common/GameGrid";
+// import React, { useEffect } from "react";
+// import useGameStore from "../../store/useGameStore";
+// import GameGrid from "../common/GameGrid";
 
-const GameSection = ({ title, viewAllLink, games, fetchGames }) => {
-  useEffect(() => {
-    fetchGames();
-  }, []);
+// const GameSection = ({ title, viewAllLink, games, fetchGames }) => {
+//   useEffect(() => {
+//     fetchGames();
+//   }, []);
 
-  return (
-    <GameGrid
-      title={title}
-      viewAllLink={viewAllLink}
-      games={games}
-      loading={games.length === 0}
-    />
-  );
-};
+//   return (
+//     <GameGrid
+//       title={title}
+//       viewAllLink={viewAllLink}
+//       games={games}
+//       loading={games.length === 0}
+//     />
+//   );
+// };
 
-export default GameSection; 
+// export default GameSection; 
